c187978c28bb4c48f4d963b93ebcb6eed56acddf
pradyumna106-ship-it <pradyumnajekumar@gmail.com>
2026-05-09 19:13:58 +0530

    shippAddress Schema updated

--- src/models/shippingAddress.js
+++ src/models/shippingAddress.js
@@ -1,73 +1,24 @@
-// models/shippingAddress.model.js
-
 import mongoose from "mongoose";
 
 const shippingAddressSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
       index: true
     },
 
-    fullName: {
-      type: String,
-      required: true,
-      trim: true
+    addressId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Address",
+      required: true
     },
 
-    phone: {
-      type: String,
-      required: true,
-      trim: true
-    },
-
-    addressLine1: {
-      type: String,
-      required: true,
-      trim: true
-    },
-
-    addressLine2: {
-      type: String,
-      trim: true,
-      default: null
-    },
-
-    landmark: {
-      type: String,
-      trim: true,
-      default: null
-    },
-
-    city: {
-      type: String,
-      required: true,
-      trim: true
-    },
-
-    state: {
-      type: String,
-      required: true,
-      trim: true
-    },
-
-    country: {
-      type: String,
-      required: true,
-      trim: true,
-      default: "India"
-    },
-
-    postalCode: {
-      type: String,
-      required: true,
-      trim: true
-    },
-
-    isDefault: {
-      type: Boolean,
-      default: false
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true
     }
   },
   {
