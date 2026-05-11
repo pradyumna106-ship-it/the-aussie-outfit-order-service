5959bee932a9fb65835ffbb77deae967359eb315
pradyumna106-ship-it <pradyumnajekumar@gmail.com>
2026-05-11 16:14:22 +0530

    removed .json() .cors()

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
   credentials: true}));
-app.options('', cors());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for form
 app.use("/uploads", express.static("uploads"));
 
